8446bfd617bcd9da295828bcc4c6488addabbdcb
khushi Sharma <khushichd.2003@gmail.com>
2023-12-24 11:27:22 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,106 @@
+// var name1 = "khushi";
+// var name2 = "priya";
+// var name3 = "simran";
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+
+// declare 3 std name in aaray
+// let name = ["khushi","priya","simran"];
+// console.log(name[0]);
+// console.log(name[1]);
+// console.log(name[2]);
+
+// array
+
+// let name =["tomato","potato","onion"];
+// console.log(name[0]);
+// console.log(name[1]);   
+// console.log(name[2]);
+
+// let start =  "khushi sharma"; 
+// console.log(start.length);
+
+// let superhero = [];
+// superhero.push("spriderman");
+// superhero.push("batman");
+// superhero.push("army");
+// console.log(superhebro);
+
+// let movie = [];
+// movie.push("spaiderman");
+// movie.push("batman");
+// movie.push("army"); 
+// for(let a = 0;a<movie.length; a++);{
+//            console.log(movie[a]);
+// }                    
+
+// function Sub (){
+//     let a=10;
+//     let b=4;
+//     console.log(a-b);
+// }
+// Sub();
+
+// function mul(a,b){
+//     return a*b;
+// }
+// console.log(mul(5,6));
+
+// function mul(a,b){
+//     return a+b;
+// }
+// console.log(mul(5,6));
+
+// let khushi = (a,b)=>{
+//     return a+b;
+// }
+// console.log(khushi(4,9));
+
+
+// var a = 3;
+// var b = 20;
+
+
+// if(a>b)
+// {
+//     console.log("a is greater");
+// }
+// else 
+// {
+//     console.log("a is not");
+// }
+
+// var a = 2;
+// var b = 3;
+// var c = (a==b);
+// if(c)
+// {
+//     console.log("a and b are equal");
+// }
+
+// var name1 = "suraj";
+// var name2 = "suraj";
+// if (name1==name2)
+// {
+//     console.log("name are equal");
+// }
+// else
+// {
+//     console.log(
+    
+N   B B "names are not equal");
+// }
+
+// let a = "raman";
+// console.log(a, a.length);
+
+let a = 2;
+let b = 3;
+let c = a+b;
+console.log("c is",c);
+
+let v = 3;
+let g = 3;
+let w = v*g;
+console.log  
